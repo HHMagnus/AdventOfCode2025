df9b9d67fc34a0adfed991e2d02f7af5c9d5eff5
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-12-01 08:20:50 +0100

    Day 1 and setup

---
+++ src/bin/day1.rs
@@ -0,0 +1,67 @@
+
+use aoc_client::{AocClient, AocResult};
+
+fn main() -> AocResult<()> {
+    let client = AocClient::builder()
+        .session_cookie_from_default_locations()?
+        .year(2025)?
+        .day(1)?
+        .build()?;
+
+    let input: String = client.get_input()?;
+
+    let answer_part1 = part1(input.clone());
+    let part1 = client.submit_answer(1, answer_part1)?;
+    println!("{:?}", part1);
+
+    let answer_part2 = part2(input.clone());
+    let part2 = client.submit_answer(2, answer_part2)?;
+    println!("{:?}", part2);
+
+    Ok(())
+}
+
+fn part1(file: String) -> String {
+    let input = parse(file);
+
+    let mut clock = 50;
+
+    let mut total = 0;
+    for i in input {
+        clock += (if i.0 { -1 } else { 1 }) * i.1;
+        clock += 10000;
+        clock %= 100;
+        if clock == 0 {
+            total += 1;
+        }
+    }
+
+    total.to_string()
+}
+
+fn parse(file: String) -> Vec<(bool, i32)> {
+    file.lines().map(|line| {
+        let left = line.starts_with('L');
+        let pos = line[1..].parse::<i32>().unwrap();
+        (left, pos)
+    }).collect::<Vec<_>>()
+}
+
+fn part2(file: String) -> String {
+    let input = parse(file);
+
+    let mut clock = 50;
+
+    let mut total = 0;
+    for i in input {
+        for _ in 0..i.1 {
+            clock += (if i.0 { -1 } else { 1 });
+            clock %= 100;
+            if clock == 0 {
+                total += 1;
+            }
+        }
+    }
+
+    total.to_string()
+}
