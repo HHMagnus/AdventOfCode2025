45becdd9c1be3bf934dd259def5ea33a03c57a00
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-12-05 13:59:03 +0100

    Submit answers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+use aoc_client::{AocClient, PuzzleDay};
 use std::fmt::Display;
 use std::fs::read_to_string;
-use aoc_client::{AocClient, AocResult, PuzzleDay};
 
 pub fn solve<PF, P, F1, F2, R1, R2>(day: PuzzleDay, parse: PF, part1: F1, part2: F2)
 where PF: Fn(&str) -> P,
@@ -10,16 +10,18 @@ where PF: Fn(&str) -> P,
       F2: Fn(P) -> R2,
       R2: Display,
 {
-    let title = format!("Day {}", day);
-    let input = get_input(day);
+    let client = aoc_client(day);
+    let input = get_input(day, &client);
     if input.is_none() {
-        println!("{}: Skipping due to no input", title);
+        println!("Day {}: Skipping due to no input", day);
         return;
     }
     let input = input.unwrap();
     let parsed = parse(&input);
     let part1_result = run_part(day, part1, "1", parsed.clone());
+    submit_part1(&client, part1_result.to_string());
     let part2_result = run_part(day, part2, "2", parsed);
+    submit_part2(&client, part2_result.to_string());
 }
 
 fn run_part<P, F, R>(day: PuzzleDay, solver: F, part: &str, input: P) -> R
@@ -33,7 +35,7 @@ where
     result
 }
 
-fn get_input(day: PuzzleDay) -> Option<String> {
+fn get_input(day: PuzzleDay, client: &AocClient) -> Option<String> {
     let file_name = format!("input/day{}.txt", day);
     let file = read_to_string(&file_name);
 
@@ -45,30 +47,47 @@ fn get_input(day: PuzzleDay) -> Option<String> {
 
     println!("Input file not found. Retrieving...");
 
-    let input = fetch_input(day);
+    let input = fetch_input(client)?;
+
+    std::fs::create_dir_all("input")
+        .expect("Failed to create dir");
+    std::fs::write(&file_name, &input)
+        .expect("Unable to write input file");
+
+    println!("Input file created: {}", file_name);
+
+    Some(input)
+}
+
+fn fetch_input(client: &AocClient) -> Option<String> {
+    let input = client.get_input();
 
     if input.is_err() {
         println!("Failed to fetch input file: {}", input.unwrap_err());
         return None;
     }
-    let output = input.unwrap();
 
-    std::fs::create_dir_all("input")
-        .expect("Failed to create dir");
-    std::fs::write(&file_name, &output)
-        .expect("Unable to write input file");
-
-    println!("Input file created: {}", file_name);
-
-    Some(output)
+    Some(input.unwrap())
 }
 
-fn fetch_input(day: PuzzleDay) -> AocResult<String> {
-    let client = AocClient::builder()
-        .session_cookie_from_default_locations()?
-        .year(2025)?
-        .day(day)?
-        .build()?;
+fn aoc_client(day: PuzzleDay) -> AocClient {
+    AocClient::builder()
+        .session_cookie_from_default_locations().expect("Failed to find session cookie")
+        .year(2025).expect("Failed to get year")
+        .day(day).expect("Failed to get day")
+        .build().expect("Failed to build client")
+}
 
-    client.get_input()
+fn submit_part1<R>(client: &AocClient, solution: R) where R: Display {
+    submit_part(client, "1", solution)
+}
+
+fn submit_part2<R>(client: &AocClient, solution: R) where R: Display {
+    submit_part(client, "2", solution)
+}
+
+fn submit_part<R>(client: &AocClient, part: &str, solution: R) where R: Display {
+    let outcome = client.submit_answer(part, solution)
+        .expect("Failed to submit answer");
+    println!("Outcome: {:?}", outcome);
 }
