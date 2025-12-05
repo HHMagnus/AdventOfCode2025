3180d1114fb1852ce28a5873a92a8111002efb64
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-12-05 12:18:52 +0100

    Day 6 setup

---
+++ src/bin/day6.rs
@@ -0,0 +1,19 @@
+use AdventOfCode2025::solve;
+
+type Parsed = Vec<usize>;
+
+fn main() {
+    solve(4, parse, part1, part2);
+}
+
+fn parse(file: &str) -> Parsed {
+    vec![]
+}
+
+fn part1(input: Parsed) -> usize {
+    0
+}
+
+fn part2(input: Parsed) -> usize {
+    0
+}
