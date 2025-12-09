45c974d4a58d1e69584b4956ac911d5693bb6387
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-12-09 19:39:42 +0100

    Day 9 part 1

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,17 +1,30 @@
 use AdventOfCode2025::solve;
 
-type Parsed = Vec<usize>;
+type Parsed = Vec<(usize, usize)>;
 
 fn main() {
     solve(9, parse, part1, part2);
 }
 
 fn parse(file: &str) -> Parsed {
-    vec![]
+    file.lines().map(|line| {
+        let mut split = line.split(',');
+        (split.next().unwrap().parse().unwrap(), split.next().unwrap().parse().unwrap())
+    }).collect()
 }
 
 fn part1(input: Parsed) -> usize {
-    0
+    let mut max = 0;
+    for i in 0..input.len() {
+        for j in i + 1..input.len() {
+            let (a, b) = input[i];
+            let (x, y) = input[j];
+            let n = (a.abs_diff(x)+1) * (b.abs_diff(y)+1);
+            max = max.max(n);
+        }
+    }
+
+    max
 }
 
 fn part2(input: Parsed) -> usize {
