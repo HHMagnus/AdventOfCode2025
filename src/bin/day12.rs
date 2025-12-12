87a713a1193c6f311220a9777738c8c0a7cdbec1
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-12-12 08:37:07 +0100

    Day 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,19 +1,49 @@
+use std::collections::HashMap;
 use AdventOfCode2025::solve;
 
-type Parsed = Vec<usize>;
+type Parsed = (HashMap<usize, Vec<(usize, usize)>>, Vec<((usize, usize), Vec<usize>)>);
 
 fn main() {
     solve(12, parse, part1, part2);
 }
 
 fn parse(file: &str) -> Parsed {
-    vec![]
+    let mut map = HashMap::new();
+    let mut vec = Vec::new();
+    for split in file.split("\n\n") {
+        let lines = split.lines().collect::<Vec<_>>();
+        if !lines[0].contains("x") {
+            let index = lines[0].replace(":", "").parse::<usize>().unwrap();
+            let vec = lines[1..].into_iter().enumerate()
+                .flat_map(|(y, line)| line.chars().enumerate()
+                    .filter(|&(_, c)| c == '#')
+                    .map(move |(x, _)| (x, y)))
+                .collect::<Vec<_>>();
+            map.insert(index, vec);
+        } else {
+            vec = lines.into_iter().map(|line| {
+                let mut split = line.split(": ");
+                let mut split2 = split.next().unwrap().split("x");
+                let x = split2.next().unwrap().parse::<usize>().unwrap();
+                let y = split2.next().unwrap().parse::<usize>().unwrap();
+                let indexes = split.next().unwrap().split(" ")
+                    .map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+                ((x, y), indexes)
+            }).collect::<Vec<_>>();
+        }
+    }
+    (map, vec)
 }
 
 fn part1(input: Parsed) -> usize {
-    0
+    input.1.into_iter().filter(|((x, y), indexes)| {
+        let size = y * x;
+
+        let total = indexes.iter().enumerate().map(|(idx, amount)| input.0[&idx].len() * amount).sum::<usize>();
+        size > total
+    }).count()
 }
 
 fn part2(input: Parsed) -> usize {
-    0
+    panic!("No part 2 for last day");
 }
